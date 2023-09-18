bd67bc978a96687484d22300021da671f3cbfdee
Richard Dalton <richard@devjoy.com>
2023-09-18 14:09:36 +0100

    Send and recenve messages over websocket

--- chat/consumers.py
+++ chat/consumers.py
@@ -9,8 +9,13 @@ class ChatConsumer(WebsocketConsumer):
             'message': 'You are now connected!'
         }))
 
-    # def receive(self, text_data=None, bytes_data=None):
-    #     pass
+    def receive(self, text_data=None, bytes_data=None):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
 
+        self.send(text_data=json.dumps({
+            'type': 'chat',
+            'message': message,
+        }))
     # def disconnect(self, code):
     #     pass
