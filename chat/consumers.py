49133e19558b6d0c78cb1bf796bf32cfdd5b514f
Richard Dalton <richard@devjoy.com>
2023-09-18 13:51:23 +0100

    Initial Commit

---
+++ chat/consumers.py
@@ -0,0 +1,16 @@
+import json
+from channels.generic.websocket import WebsocketConsumer
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.accept()
+        self.send(text_data=json.dumps({
+            'type': 'connection_established',
+            'message': 'You are now connected!'
+        }))
+
+    # def receive(self, text_data=None, bytes_data=None):
+    #     pass
+
+    # def disconnect(self, code):
+    #     pass
